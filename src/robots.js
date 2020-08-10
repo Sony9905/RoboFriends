5afd15f820670ddb547454c56a02704cfbdb39e1
Sony <1sony9905@gmail.com>
2020-08-10 10:50:34 -0400

    adding files

---
+++ src/robots.js
@@ -0,0 +1,64 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Sony Singh',
+    username: 'Sony',
+    email: 'Sonis@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Derek Jiang',
+    username: 'Derek',
+    email: 'Derek@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Gurveer Singh',
+    username: 'Gerry',
+    email: 'Gurveer@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Thammie L. Quach',
+    username: 'Thammie',
+    email: 'ThammielaineQ@laine.com'
+  },
+  {
+    id: 5,
+    name: 'Zeng Lin',
+    username: 'Zeng',
+    email: 'Zeng@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Adonis Zheng',
+    username: 'Adonis',
+    email: 'Aony@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Linda Quach',
+    username: 'Linda.Q',
+    email: 'Linda@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Thomas Quach',
+    username: 'TQ',
+    email: 'Thomas@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Goku',
+    username: '9000',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Rajveer Singh',
+    username: 'Gullotine',
+    email: 'Raj@karina.biz'
+  }
+];
+
+export default robots;
